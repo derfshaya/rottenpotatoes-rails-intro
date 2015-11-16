672ea77e9107a14b8cded1a5c33e0c659e052fbc
Derf Shaya <derfshaya@gmail.com>
2015-11-16 00:26:57 +0000

    bugfix attempt

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     # if first time, check all ratings
-    params[:ratings] = Movie.all_ratings if params[:ratings].nil? && session[:ratings].nil?
+    params[:ratings] = Movie.all_ratings_hash if params[:ratings].nil? && session[:ratings].nil?
     
     session[:ratings] = params[:ratings] if params[:ratings]
     session[:sort_by] = params[:sort_by] if params[:sort_by]
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         ['G', 'PG', 'PG-13', 'R']
     end
+    
+    def self.all_ratings_hash
+        hash = Hash.new
+        all_ratings.each do |rating|
+            hash[rating] = "1"
+        end
+        hash
+    end
 end
